65db7450fdcd79889a0b2405dd9e4f2cfd931723
Basedfloppa <113017705+Basedfloppa@users.noreply.github.com>
2024-05-02 20:03:48 +0300

    cleanup

--- Levels/World1.gd
+++ Levels/World1.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-@export var BackGround_path = "res://Sprites/backgrounds/World_1_Background.tscn"
+@export var BackGround_path = "res://Sprites/backgrounds/grass.png"
 var Player_path = Global.ChoosenCharacter
 @export var Gui_path = "res://Utility/GUI/world_gui.tscn"
 @export var EnemySpawner_path = "res://Utility/World/enemy_spawner.tscn"
 
-var BackGround = load(BackGround_path).instantiate()
+var BackGround = Sprite2D.new()
 var Player = load(Player_path).instantiate()
 var Gui = load(Gui_path).instantiate()
 var EnemySpawner = load(EnemySpawner_path).instantiate()
 
 
 func _ready():
+	BackGround.texture = load(BackGround_path)
+	BackGround.texture_repeat = CanvasItem.TEXTURE_REPEAT_ENABLED
+	BackGround.region_enabled = true
+	BackGround.region_rect = Rect2(Vector2.ZERO,Vector2(1000000,1000000))
 	add_child(BackGround)
 	
 	var start_exp = load("res://Loot/exp.tscn").instantiate()
